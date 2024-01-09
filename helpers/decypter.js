7c5e552f340296a676b11d738e14abe702067a66
razhaziq <razhaziq@gmail.com>
2024-01-09 12:02:13 +0800

    aws KMS

--- helpers/decypter.js
+++ helpers/decypter.js
@@ -1,29 +1,47 @@
+require("dotenv").config({ path: "../.env" });
+const crypto = require("crypto");
+const algorithm = "aes-256-cbc";
+const AWS = require("aws-sdk");
+const kms = new AWS.KMS();
 
-require('dotenv').config({ path: "../.env" })
-const crypto = require('crypto');
-const algorithm = 'aes-256-cbc';
-const key = Buffer.from(process.env.KEY, 'hex');
-const iv = Buffer.from(process.env.IV_KEY, 'hex');
+const iv = Buffer.from(process.env.IV_KEY, "hex");
+const keyAlias = 'apedBotBackendKEY';
 
+async function getKMSData(alias) {
+  const params = {
+    KeyId: alias,
+  };
 
-module.exports.decryptor = (hash) => {
-    
-    const decipher = crypto.createDecipheriv(algorithm, key, iv);
-    let decryptedData = decipher.update(hash, 'hex', 'utf8');
-    decryptedData += decipher.final('utf8');
-
-    return decryptedData;
-
-
+  const response = await kms.describeKey(params).promise();
+  return response.KeyMetadata.Arn;
 }
 
+async function getKeyFromKMS(alias) {
+  const keyArn = await getKMSData(alias);
+  const params = {
+    KeyId: keyArn,
+  };
 
-module.exports.encryptor = (hash) => {
-
-    const cipher = crypto.createCipheriv(algorithm, key, iv);
-    let encryptedKey = cipher.update(hash, "utf-8", "hex");
-    encryptedKey += cipher.final("hex");
-
-    return encryptedKey;
-    
+  const response = await kms.decrypt(params).promise();
+  return response.Plaintext;
 }
+
+module.exports.decryptor = async (hash) => {
+  const key = await getKeyFromKMS(keyAlias);
+
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+  let decryptedData = decipher.update(hash, "hex", "utf8");
+  decryptedData += decipher.final("utf8");
+
+  return decryptedData;
+};
+
+module.exports.encryptor = async (hash) => {
+  const key = await getKeyFromKMS(keyAlias);
+
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encryptedKey = cipher.update(hash, "utf-8", "hex");
+  encryptedKey += cipher.final("hex");
+
+  return encryptedKey;
+};
