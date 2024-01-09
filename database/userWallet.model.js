7f76c30da70a5158b420c258d8699fd1d2e6c272
razhaziq <razhaziq@gmail.com>
2024-01-09 13:04:41 +0800

    wallet DataTypes text

--- database/userWallet.model.js
+++ database/userWallet.model.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
     const UserWallet = sequelize.define('wallet', {
         publicKey: DataTypes.STRING,
-        privateKey: DataTypes.STRING,
+        privateKey: DataTypes.TEXT,
         walletOwner: DataTypes.STRING
     });
 
